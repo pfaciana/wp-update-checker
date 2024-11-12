f917ffd57002d8d7fd205c91eea92d638fc80bee
Phil Faciana <pfaciana@gmail.com>
2024-11-11 21:28:56 -0500

    fix: init `composer-smart-autoloader` in load.php

--- load.php
+++ load.php
@@ -4,6 +4,10 @@ if ( !defined( 'WP_THEME_DIR' ) ) {
 	define( 'WP_THEME_DIR', WP_CONTENT_DIR . '/themes' ); // Full path, no trailing slash.
 }
 
+add_action( 'plugins_loaded', function () {
+	\Render\Autoload\ClassLoader::getInstance();
+}, PHP_INT_MIN );
+
 add_action( 'init', function () {
 	if ( !did_action( 'rpuc/init' ) ) {
 		do_action( 'rpuc/init' );

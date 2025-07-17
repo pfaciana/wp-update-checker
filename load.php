b7af1991f1eb9138135cc151a5a3b08a669bc06c
Phil Faciana <pfaciana@gmail.com>
2025-07-17 16:46:45 -0400

    fix: checked for WordPress functions to be defined
    - Primarily avoid friction when unit testing

--- load.php
+++ load.php
@@ -1,15 +1,17 @@
 <?php
 
-if ( !defined( 'WP_THEME_DIR' ) ) {
+if ( !defined( 'WP_THEME_DIR' ) && defined( 'WP_CONTENT_DIR' ) ) {
 	define( 'WP_THEME_DIR', WP_CONTENT_DIR . '/themes' ); // Full path, no trailing slash.
 }
 
-add_action( 'plugins_loaded', function () {
-	\Render\Autoload\ClassLoader::getInstance();
-}, PHP_INT_MIN );
+if ( function_exists( 'add_action' ) ) {
+	add_action( 'plugins_loaded', function () {
+		\Render\Autoload\ClassLoader::getInstance();
+	}, PHP_INT_MIN );
 
-add_action( 'init', function () {
-	if ( !did_action( 'rpuc/init' ) ) {
-		do_action( 'rpuc/init' );
-	}
-}, -99 );
+	add_action( 'init', function () {
+		if ( !did_action( 'rpuc/init' ) ) {
+			do_action( 'rpuc/init' );
+		}
+	}, -99 );
+}
